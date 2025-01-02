0194a910d40afac50be140ce724f50b6e81ba784
Qiuyuan Chen <qche0055@student.monash.edu>
2025-01-02 23:11:10 +1100

    Add Vignette detailed description

---
+++ R/customs_encounter.R
@@ -0,0 +1,15 @@
+#' @description CBP response data
+#' @format A data frame with (68815)rows and (12)columns
+#' @source https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2024/2024 - 11 - 26/cbp_resp.csv
+#' @examples
+#' data(cbp_resp)
+#' head(cbp_resp)
+"cbp_resp"
+
+#' @description CBP state data
+#' @format A data frame with (54939)rows and (9) columns
+#' @source https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2024/2024 - 11 - 26/cbp_state.csv
+#' @examples
+#' data(cbp_state)
+#' head(cbp_state)
+"cbp_state"
--- data-raw/customs_encounters.R
+++ data-raw/customs_encounters.R
@@ -14,18 +14,4 @@ usethis::use_data(cbp_resp, overwrite = TRUE)
 #Save the CBP state data to the package
 usethis::use_data(cbp_state, overwrite = TRUE)
 
-#' @description CBP response data
-#' @format A data frame with (68815)rows and (12)columns
-#' @source https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2024/2024 - 11 - 26/cbp_resp.csv
-#' @examples
-#' data(cbp_resp)
-#' head(cbp_resp)
-"cbp_resp"
 
-#' @description CBP state data
-#' @format A data frame with (54939)rows and (9) columns
-#' @source https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2024/2024 - 11 - 26/cbp_state.csv
-#' @examples
-#' data(cbp_state)
-#' head(cbp_state)
-"cbp_state"
